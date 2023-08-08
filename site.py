258952cebb95f9bd7277ba7ce84ee8e762d9c6ee
nathanevaans <70093608+nathanevaans@users.noreply.github.com>
2023-08-08 07:41:17 +0100

    Update site.py

--- site.py
+++ site.py
@@ -15,34 +15,56 @@ tickers = {
     'TSLA': 'Tesla, Inc.'
 }
 
+exchanges = {
+    "NYSE": {"name": "New York Stock Exchange", "location": "USA"},
+    "NASDAQ": {"name": "NASDAQ", "location": "USA"},
+    "TSE": {"name": "Tokyo Stock Exchange", "location": "Japan"},
+    "SSE": {"name": "Shanghai Stock Exchange", "location": "China"},
+    "HKEX": {"name": "Hong Kong Stock Exchange", "location": "Hong Kong"},
+    "Euronext": {"name": "Euronext", "location": "Europe"},
+    "LSE": {"name": "London Stock Exchange", "location": "UK"},
+    "SZSE": {"name": "Shenzhen Stock Exchange", "location": "China"},
+    "TSX": {"name": "Toronto Stock Exchange", "location": "Canada"},
+    "FWB": {"name": "Frankfurt Stock Exchange", "location": "Germany"}
+}
+
 st.write("""
 # Forecaster
 """)
 
 container = st.container()
-ticker = container.text_input(label='## Stock Ticker').strip()
+exchange = container.selectbox(
+    label='Exchange',
+    options=['Select an Exchange'] + [key for key in exchanges.keys()]
+)
+ticker = container.text_input(label='Stock Ticker').strip()
 model = container.selectbox(
-        label='## Model',
+        label='Model',
         options=[
             'None',
             'ARMA',
             'ARIMA',
             'SARIMA',
-            'Harmonic Regression'
+            'Harmonic Regression',
+            'Fourier Analysis'
         ]
     )
 
-if ticker == '':
-    st.write('')
-elif ticker not in tickers:
-    st.error("Ticker not available")
-else:
-    st.success(tickers[ticker])
-    if model != 'None':
-        # do logic and alter graph
-        forecast_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
-        st.line_chart(forecast_data, use_container_width=True)
+# create a dictionary of exchanges that contain records of tickers and stuff
+if exchange != 'Select an Exchange':
+    if ticker == '':
+        st.write('')
+    elif ticker not in tickers:
+        st.error("Ticker not available")
     else:
-        # un edtited graph
-        forecast_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
-        st.line_chart(forecast_data, use_container_width=True)
+        st.success(tickers[ticker])
+        if model != 'None':
+            # do logic and alter graph
+            forecast_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+            st.line_chart(forecast_data, use_container_width=True)
+        else:
+            # un edtited graph
+            forecast_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+            st.line_chart(forecast_data, use_container_width=True)
+
+# perhaps use a form to with text input and two select boxes and once all done you press forecast button
